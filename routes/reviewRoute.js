0581b9e6e5cf3957586542c58dea3fcae3f369e6
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-03-15 14:14:23 -0600

    deleted email from req

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -19,19 +19,20 @@ router.post('/', function(req, res){
 
   userRoute().insert({
     email: req.body.email
-  }, ['email','id']).then(
+  }, ['email','id']).then(function(result){
+
     reviewRoute().insert({
       body: req.body.body,
       rating: req.body.rating,
       created_at: req.body.created_at,
       recipe_id: req.body.recipe_id,
-      email: req.body.email,
+      // email: req.body.email,
       user_id: knex('user').where('email', req.body.email).select('id')
       // email: req.body.email,
     }, ['body', 'rating', 'created_at', 'id', 'recipe_id', 'user_id']).then(function(result){
       res.json(result);
-    })
-  );
+    });
+  });
 });
 
 //review GETALL//
